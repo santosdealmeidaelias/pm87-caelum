9d0f0a18ddb108e5c4f8389558e1f6634de89d0b
unknown <santosdealmeidaelias@gmail.com>
2015-08-17 13:04:14 -0300

    mais uma mensagem

--- Teste.java
+++ Teste.java
@@ -1,5 +1,6 @@
 public class Teste {
 	public static void main(String[] args) {
-		System.out.println("Exemplo de teste uso do git);
+		System.out.println("Exemplo de teste uso do git");
+		System.out.println("Trabalhando com Branches");
 	}
 }
